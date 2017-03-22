5ab7c0eaef8a2cb7968a4d8961da0ee4f7c5920b
student <student@172.26.0.185>
2017-03-22 14:01:38 -0600

    Committed

---
+++ src/darkforces/DarkForces.java
@@ -0,0 +1,17 @@
+
+package darkforces;
+
+
+public class DarkForces {
+    private static Object PrettyPrompts;
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Enter a username: ");
+        
+        
+    }
+    
+}
---
+++ src/darkforces/PrettyPrompts.java
@@ -0,0 +1,19 @@
+
+package darkforces;
+
+import java.util.Scanner;
+
+
+public class PrettyPrompts {
+    Scanner name = new Scanner(System.in);
+    
+    
+    
+    int first = name.nextInt();
+    
+    
+    
+    
+    
+    
+}
